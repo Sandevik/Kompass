f70ae76a6d86cda65821a1f830b9c78f7d494843
[Simon] <[simon.sandevik@outlook.com]>
2023-10-09 13:17:39 +0200

    score based on keywords

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -2,57 +2,49 @@ use std::collections::HashMap;
 use std::fs;
 use std::future::Future;
 use std::hash::Hash;
+use quick_xml::events::attributes::Attribute;
 use reqwest::{self, Client};
 use quick_xml;
 use quick_xml::events::Event;
 use quick_xml::reader::Reader;
 
-struct Website {
-    content: String,
-    links: Vec<String>,
-    word_dictionary: HashMap<String, u16>
+#[derive(Debug)]
+struct Link {
+    site_url: String,
+    site_score: Option<u32>,
+}
+
+#[derive(Debug)]
+struct Site {
+    internal_links: Option<Vec<Link>>,
+    external_links: Option<Vec<Link>>,
+    word_dictionary: HashMap<String, u16>,
+    score: u16,
+}
+
+impl Site {
+    pub fn new() -> Self {
+        Site {internal_links: None, external_links: None, word_dictionary: HashMap::new(), score: 0 }
+    }
 }
 
 #[tokio::main]
 async fn main() {
 
+    const URL: &str = "https://younify.xyz";
+
     let keywords = load_keywords();
 
     let request_client = reqwest::Client::new();
-    let raw_html = access_website(&request_client, "https://younify.xyz").await;
-
-    let mut reader = Reader::from_str(&raw_html);
-    reader.trim_text(true);
-
-    let mut buf = Vec::new();
-    let mut extracted_text = String::new();
-
-    loop {
-        match reader.read_event_into(&mut buf) {
-            Err(e) => {println!("ERROR: Could not read event : {e}")},
-            Ok(Event::Eof) => break,
-            Ok(Event::Text(e)) => {
-                extracted_text.push(' ');
-                extracted_text.push_str(&e.unescape().unwrap().into_owned());
-            },
-            _ => ()
-        }
-    }
-
-    let mut word_dictionary = HashMap::<String, u16>::new();
-
-    for word in extracted_text.split(' ') {
-        if word_dictionary.contains_key(word) {
-            word_dictionary.insert(word.to_lowercase(), word_dictionary[word] + 1);
-        } else {
-            if word != "\n" || word != "-" || word != "" {
-                word_dictionary.insert(word.to_lowercase(), 1);
-            }
-        }
-    }
+    
+    let mut site = extract_values(&request_client, URL, &keywords).await;
 
 
-    println!("{keywords:#?}");
+
+
+
+
+    println!("{site:#?}");
 }
 
 async fn access_website(client: &Client, url: &str) -> String {
@@ -68,3 +60,87 @@ fn load_keywords() -> Vec<String> {
     } 
     key_words
 }
+
+async fn extract_values<'a>(request_client: &Client, url: &str, keywords: &Vec<String>) -> Site {
+
+    let raw_html = access_website(&request_client, url).await;
+
+    let mut reader = Reader::from_str(&raw_html);
+    reader.trim_text(true);
+
+    let mut buf = Vec::new();
+    let mut extracted_text = String::new();
+    let mut internal_links = Vec::<String>::new();
+    let mut external_links = Vec::<String>::new();
+
+
+
+    loop {
+        match reader.read_event_into(&mut buf) {
+            Err(e) => {println!("ERROR: Could not read event : {e}")},
+            Ok(Event::Eof) => break,
+            Ok(Event::Text(e)) => {
+                extracted_text.push(' ');
+                extracted_text.push_str(&e.unescape().unwrap().into_owned());
+            },
+            Ok(Event::Start(e)) => {
+                /* match e.name().as_ref() {
+                    b"a" => {
+                        e.attributes().for_each(|attribute| {
+                            if attribute.unwrap().key. {
+
+                            }
+                        })
+                    }
+                } */
+                continue;
+            }
+            _ => ()
+        }
+    }
+
+    let mut word_dictionary = HashMap::<String, u16>::new();
+
+    for word in extracted_text.split(' ') {
+        for keyword in keywords {
+            if word == keyword && word_dictionary.contains_key(word) {
+                word_dictionary.insert(word.to_lowercase(), word_dictionary[word] + 1);
+            } else if word == keyword && !word_dictionary.contains_key(word) && word != "\n" && word != "-" && word != "" {
+                word_dictionary.insert(word.to_lowercase(), 1);
+            }
+        }
+    }
+
+
+    let mut score: u16 = 0;
+
+    for (_, count) in &word_dictionary {
+        score += count;
+    }
+
+
+    let mut site = Site::new();
+
+    site.word_dictionary = word_dictionary;
+
+    if external_links.len() > 0 {
+        score += 2 * external_links.len() as u16;
+        let external: Vec<Link> = external_links.iter().map(|link| {Link {site_url: link.to_owned(), site_score: None}}).collect();
+        site.external_links = Some(external)
+    } else {
+        site.external_links = None;
+    }
+    if internal_links.len() > 0 {
+        let internal: Vec<Link> = internal_links.iter().map(|link| {Link {site_url: link.to_owned(), site_score: None}}).collect();
+        site.internal_links = Some(internal)
+    } else {
+        site.internal_links = None;
+    }
+
+
+    site.score = score;
+
+    site
+
+}
+
