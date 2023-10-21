575918bba87889ee5698ea1f1fa6609f04ba6d9a
[Simon] <[simon.sandevik@outlook.com]>
2023-10-21 17:46:09 +0200

    title and favicon

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "62066b8f3d01d246"; }
+/******/ 		__webpack_require__.h = function() { return "5fc594f2820da7c6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,6 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Kompass | Find your way</title>
+      <link rel="icon" href="./Kompass2d.png" />
       <Head />
       <body>
         <Main />
